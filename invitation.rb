d6ee0eb435146bf6ca4eeeeea8508e2ecd142b00
MissKobayashi <priscin2002@gmail.com>
2018-07-09 18:04:57 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -4,4 +4,8 @@
 
 puts "Dear Heromine,"
 
-puts "You have graciously been invited to the Best Halloween Party Ever. "
+puts "You have graciously been invited to the Best Halloween Party Ever. The party will be on October 31 at 6 pm. Please RSVP no later then October 30."
+
+puts "Sincerly,"
+
+puts "Harry Potter"
