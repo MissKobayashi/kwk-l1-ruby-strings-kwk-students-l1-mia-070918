26f33b4401405c1aae1982f0b482d2733dde7551
MissKobayashi <priscin2002@gmail.com>
2018-07-09 18:06:19 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,7 +3,7 @@
 # Try starting out with puts'ing a string.
 
 puts "Dear Heromine,"
-
+puts " "
 puts "You have graciously been invited to the Best Halloween Party Ever. The party will be on October 31 at 6 pm. Please RSVP no later then October 30."
 
 puts "Sincerly,"
